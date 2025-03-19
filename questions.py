df2f2a93384b20881371632dd85a772c4e8205fd
Ramiro <buente13@gmail.com>
2025-03-19 20:34:10 -0300

    commit ejercicio 1 y 2

--- questions.py
+++ questions.py
@@ -21,6 +21,8 @@ answers = [
 ]
 # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
+#inicializamos el puntaje en 0
+point=0
 # El usuario deberá contestar 3 preguntas
 for _ in range(3):
     # Se selecciona una pregunta aleatoria
@@ -45,14 +47,18 @@ for _ in range(3):
         user_answer= int(num) -1
         # Se verifica si la respuesta es correcta
         if user_answer == correct_answers_index[question_index]:
+            point+=1
             print("¡Correcto!")
             break
-        
+        else:
+            point-=0.5
+            
     else:
         # Si el usuario no responde correctamente después de 2 intentos,
         # se muestra la respuesta correcta
         print("Incorrecto. La respuesta correcta es:")
         print(answers[question_index]
 [correct_answers_index[question_index]])
-# Se imprime un blanco al final de la pregunta
+print('el puntaje final es',point)
+#Se imprime un blanco al final de la pregunta
 print()
