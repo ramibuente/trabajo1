9cdc83561c757b111d5903c010a4440c845341b2
Ramiro <buente13@gmail.com>
2025-03-20 22:58:44 -0300

    punto 3, agrego el questions_to_ask

--- questions.py
+++ questions.py
@@ -21,16 +21,15 @@ answers = [
 ]
 # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
+#unificamos las 3 listas en 1
+questions_to_ask = random.choices(list(zip(questions,answers, correct_answers_index)), k=3)
 #inicializamos el puntaje en 0
 point=0
 # El usuario deberá contestar 3 preguntas
-for _ in range(3):
-    # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
-
+for questions,answers,correct_answers_index in questions_to_ask:
     # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+    print(questions)
+    for i, answer in enumerate(answers):
         print(f"{i + 1}. {answer}")
     # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
@@ -46,7 +45,7 @@ for _ in range(3):
             exit(1)
         user_answer= int(num) -1
         # Se verifica si la respuesta es correcta
-        if user_answer == correct_answers_index[question_index]:
+        if user_answer == correct_answers_index:
             point+=1
             print("¡Correcto!")
             break
@@ -57,8 +56,8 @@ for _ in range(3):
         # Si el usuario no responde correctamente después de 2 intentos,
         # se muestra la respuesta correcta
         print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index]
-[correct_answers_index[question_index]])
+        print(answers
+[correct_answers_index])
 print('el puntaje final es',point)
 #Se imprime un blanco al final de la pregunta
 print()
